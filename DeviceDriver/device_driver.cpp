39342069deb6f327b39674086c759dfb2b24fb6c
dongig.shin <dongig.shin@samsung.com>
2025-07-03 16:37:44 +0900

    dd read/write

--- DeviceDriver/device_driver.cpp
+++ DeviceDriver/device_driver.cpp
@@ -1,4 +1,7 @@
 #include "device_driver.h"
+#include <iostream>
+class ReadFiveTimeFail : public std::exception {};
+class WriteWhenRead0xFF : public std::exception {};
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {
@@ -7,11 +10,27 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 int DeviceDriver::read(long address)
 {
     // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int ret = (int)(m_hardware->read(address));
+    checkPostcondition(ret, address);
+    return ret;
+}
+
+void DeviceDriver::checkPostcondition(int& ret, long address)
+{
+    int cmp;
+    for (int i = 0; i < 4; i++) {
+        cmp = (int)(m_hardware->read(address));
+        if (cmp != ret)
+            throw std::exception();
+    }
+    return;
 }
 
 void DeviceDriver::write(long address, int data)
 {
-    // TODO: implement this method
+    int testValue = (int)(m_hardware->read(address));
+    if (testValue != 0xFF) {
+        throw WriteWhenRead0xFF();
+    }
     m_hardware->write(address, (unsigned char)data);
 }
